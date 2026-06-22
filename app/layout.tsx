44f9bfe76e1be3583686f82368af126f57ab0186
unknown <dev4@sioxglobal.com>
2026-06-22 12:13:04 -0400

    feat: speed insights added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import { LuxuryCursor } from "@/components/luxury-cursor";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,6 +55,7 @@ export default function RootLayout({
         {children}
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
