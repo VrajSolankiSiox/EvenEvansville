9b988d15c00ab4509e920ffd2389ab5c09bdb147
unknown <dev4@sioxglobal.com>
2026-06-22 11:12:43 -0400

    fix: fixed robots txt

--- app/robots.ts
+++ app/robots.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = "https://evansvilleinnandsuites.com"; // Update this when you have your final domain
+  const baseUrl = "https://www.evansvilleinn.com";  
 
   return {
     rules: {
